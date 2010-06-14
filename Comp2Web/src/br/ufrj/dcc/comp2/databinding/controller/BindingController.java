e325c689c7dddb421bc9636404b71f267f8e2474
jhccosta <jhccosta@d1817a05-fa36-5cee-7fe0-c65e11beafc6>
2010-06-14 22:05:05 +0000

    Projeto enviado enviado para o servidor! Mudar as configurações do persistence.xml de SQL Server 2005!

---
+++ Comp2Web/src/br/ufrj/dcc/comp2/databinding/controller/BindingController.java
@@ -0,0 +1,39 @@
+package br.ufrj.dcc.comp2.databinding.controller;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+
+import org.zkoss.zkplus.jpa.JpaUtil;
+
+import br.ufrj.dcc.comp2.databinding.model.Model;
+
+public class BindingController {
+
+	public <T extends Model> void addPerson(T entity) {
+		EntityManager em = JpaUtil.getEntityManager();
+		em.persist(entity);
+	}
+
+	public <T extends Model> void update(T selected) {
+		EntityManager em = JpaUtil.getEntityManager();
+		em.merge(selected);
+	}
+
+	@SuppressWarnings("unchecked")
+	public <T extends Model> List<T> getAll(Class<T> clazz) {
+		EntityManager em = JpaUtil.getEntityManager();
+		return (List<T>) em.createQuery("SELECT x FROM " + clazz.getName() + " x").getResultList();
+	}
+
+	public <T extends Model> T getById(Class<T> clazz, Integer id) {
+		EntityManager em = JpaUtil.getEntityManager();
+		return em.find(clazz, id);
+	}
+
+	public <T extends Model> void remove(T entity) {
+		EntityManager em = JpaUtil.getEntityManager();
+		em.remove(em.merge(entity));
+	}
+	
+}
